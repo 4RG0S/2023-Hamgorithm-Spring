c29b5548b4a1c8b169bd5b815dffd63e32afed7a
양준하 <100835168+Ori-Gui@users.noreply.github.com>
2023-03-27 21:05:48 +0900

    2023.03.27.py

---
+++ 201902716/2023.03.27.py
@@ -0,0 +1,11 @@
+import math
+
+def main():
+    case = int(input())
+    for _ in range(case):
+        n, m = map(int, input().split(' '))
+        bridge = math.factorial(m) // (math.factorial(n) * math.factorial(m - n))
+        print(bridge)
+    
+if __name__ == '__main__':
+    main()
