3ca9ca9f10891b65f79a3753b959a13348ef239c
yongbeompark <105450053+yongbeompark@users.noreply.github.com>
2023-03-27 23:19:45 +0900

    Add files via upload

---
+++ 8958.py
@@ -0,0 +1,22 @@
+a = int(input())
+input_list = list(input() for _ in range(a))
+
+x = 0
+y = 0
+score = []
+
+for i in range(len(input_list)) :
+    for j in range(len(input_list[i])) :
+        if input_list[i][j] == 'O' :
+            x += 1
+            y += 1
+            score.append(x)
+            
+        elif input_list[i][j] == 'X' :
+            x = 0
+            y += 1
+            
+        if j + 1 == len(input_list[i]) :
+            print(sum(score))
+            score = []
+            x = 0
