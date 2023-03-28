c33270ca3c767ba10af0fb3be26a26fe2e5a7e9c
양준하 <100835168+Ori-Gui@users.noreply.github.com>
2023-03-28 14:45:14 +0900

    2023.03.28_1064.py

---
+++ 2023.03.28_1064.py
@@ -0,0 +1,16 @@
+p = list()
+
+def main():
+    xa, ya, xb, yb, xc, yc = map(float, input().split())
+    p.append((abs(xa-xb)**2 + abs(ya-yb)**2)**0.5)
+    p.append((abs(xb-xc)**2 + abs(yb-yc)**2)**0.5)
+    p.append((abs(xc-xa)**2 + abs(yc-ya)**2)**0.5)
+    answer = (max(p) - min(p))*2
+    
+    if (xa-xb)*(ya-yc) == (xa-xc)*(ya-yb):
+        print(-1.0)
+    else:
+        print(answer)
+
+if __name__ == '__main__':
+    main()
