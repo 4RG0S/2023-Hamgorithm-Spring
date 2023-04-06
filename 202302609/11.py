67fef2a0cb9019a6ac5e85a7ade5d14b89d06da4
Neo6424 <129708429+Neo6424@users.noreply.github.com>
2023-04-06 14:23:12 +0900

    2023.04.06

--- 202302609/11.py
+++ 202302609/11.py
@@ -1,4 +1,11 @@
-n = list(map(int, input().split()))
-print(n)
-
-222
+while True:
+    a, b = map(int, input().split())
+    if a == 0 and b == 0:
+        break
+    
+    if b % a == 0:
+        print("factor")
+    elif a % b == 0:
+        print("multiple")
+    else:
+        print("neither")
