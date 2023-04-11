e9d42eb8c27ae5dfaa062c4354bd6bade03ec39c
horcrux-jin <76608042+horcrux-jin@users.noreply.github.com>
2023-04-11 11:46:44 +0900

    20230411_10951_나소진

---
+++ 202302547/20230411_10951_나소진.java
@@ -0,0 +1,18 @@
+package backjoon;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args){
+		
+		Scanner sc = new Scanner(System.in);
+
+		while(sc.hasNextInt()) {
+			int num1 = sc.nextInt();
+			int num2 = sc.nextInt();
+			System.out.printf("%d\n", num1 + num2);
+		}
+	}
+}
+
