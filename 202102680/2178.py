1ac95c8328308cd962a5dc2106a04da7df660308
duswnwn <rita27@naver.com>
2023-04-13 19:19:07 +0900

    23/04/13 미로탐색

---
+++ 202102680/2178.py
@@ -0,0 +1,35 @@
+import sys
+from collections import deque
+
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+graph = []
+
+for _ in range(n):
+    graph.append(list(map(int, input().rstrip())))  # readline의 경우 맨 뒤에 '\n'까지 입력받으므로 제거해줘야 함
+
+# 상하좌우
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+
+
+def bfs(x, y):
+    queue = deque()
+    queue.append((x, y))
+
+    while queue:
+        x, y = queue.popleft()
+
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+
+            if 0 <= nx < n and 0 <= ny < m and graph[nx][ny] == 1:
+                queue.append((nx, ny))
+                graph[nx][ny] = graph[x][y] + 1
+
+    return graph[n - 1][m - 1]
+
+
+print(bfs(0, 0))
