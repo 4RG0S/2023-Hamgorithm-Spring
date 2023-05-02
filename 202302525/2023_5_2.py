d7214e9c0f6aaf74d859be6e42a5cc171f3591c7
rlqja <rlqja1220@gmail.com>
2023-05-02 15:16:14 +0900

    2023_5_2

--- 202302525/2023_5_2.py
+++ 202302525/2023_5_2.py
@@ -1,12 +1,51 @@
-n, m = map(int, input().split())
+l_result = [0] * 10
+u_result = [0] * 10
+l, u = map(int, input().split())
+l -= 1
+digit = 1
+result = 0
 
-result = [0] * n
+while True:
+    while l % 10 != 9: #떨이 뺌
+        l_list = list(map(int, str(l)))
+        for i in range(len(l_list)):
+            l_result[l_list[i]] += digit
+        l -= 1
 
-for _ in range(m):
-    a, b, c = map(int, input().split())
+    if l >= 10:
+        for i in range(10):
+            l_result[i] += (l // 10 +1) * digit
 
-    for i in range(a,b+1):
-        result[i-1] = c
+    else:
+        for i in range(1, l+1):
+            l_result[i] += digit
+        break
 
-for i in range(len(result)):
-    print(result[i])
+    digit *= 10
+    l //= 10
+
+digit = 1
+while True:
+    while u % 10 != 9: #떨이 뺌
+        u_list = list(map(int, str(u)))
+        for i in range(len(u_list)):
+            u_result[u_list[i]] += digit
+        u -= 1
+
+    if u >= 10:
+        for i in range(10):
+            u_result[i] += (u // 10 +1) * digit
+
+    else:
+        for i in range(1, u+1):
+            u_result[i] += digit
+        break
+        
+
+    digit *= 10
+    u //= 10
+
+for i in range(10):
+    result += (u_result[i] * i) - (l_result[i] * i)
+
+print(result)
