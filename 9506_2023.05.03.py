77bc7c00b6a91432a5fdca02c40ae2adf4332eeb
byegu <100301088+byegu@users.noreply.github.com>
2023-05-03 23:55:18 +0900

    9506_2023.05.03.py

---
+++ 9506_2023.05.03.py
@@ -0,0 +1,24 @@
+def suma(N):
+    temp = 0;
+    t = 0;
+    temp2 = []
+    temp3 = 0;
+    for i in range(1, N):
+        if N % i == 0:
+            temp += i
+            temp2.append(i)
+    if temp == N:
+        print(N, "=", end=" ")
+        for j in range(len(temp2)):
+            print(temp2[j], end = " ")
+            if j < len(temp2) - 1:
+                print("+", end = " ")
+        print("")
+    elif N > 0 and temp != N:
+        print(N, "is NOT perfect.")
+
+while True:
+    n = int(input())
+    suma(n)
+    if n == -1:
+        break;
