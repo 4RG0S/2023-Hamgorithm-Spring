72066dbfe3a665e68014628ec115545077b04f29
janghan <95536976+janghana@users.noreply.github.com>
2023-05-09 13:21:30 +0900

    Upload Q2669.py file

---
+++ 201902745/Q2669.py
@@ -0,0 +1,10 @@
+box = [[0 for _ in range(101)] for _ in range(101)]
+for _ in range(4):
+    x1, y1, x2, y2 = map(int, input().split())
+    for i in range(x1, x2):
+        for j in range(y1, y2):
+            box[i][j] = 1
+result = 0
+for width in box:
+    result += sum(width)
+print(result)
