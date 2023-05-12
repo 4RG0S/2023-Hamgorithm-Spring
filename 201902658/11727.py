5713f57ac2826f67aad064fa37154b27f6c80a63
Kimdaemin <56399681+daemin-kim@users.noreply.github.com>
2023-05-12 13:40:46 +0900

    solve 11727

---
+++ 201902658/11727.py
@@ -0,0 +1,9 @@
+if __name__ == "__main__":
+    N = int(input())
+    dp = [0] * (1001)
+    dp[1] = 1
+    dp[2] = 3
+    for i in range(3, 1001):
+        dp[i] = dp[i - 1] + (2 * dp[i - 2])
+
+    print(dp[N] % 10007)
