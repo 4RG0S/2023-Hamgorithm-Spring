e9f35aade9f911530f1fdce8a0be98105ba7c755
haaaanung <129250257+haaaanung@users.noreply.github.com>
2023-05-26 10:30:21 +0900

    2023.05.26_202302635_한웅.py

---
+++ 202302635/2023.05.26_202302635_한웅.py
@@ -0,0 +1,16 @@
+X = int(input())
+line = 1
+
+while X > line :
+    X -= line
+    line += 1
+
+if line % 2 == 0 :
+    a = X
+    b = line - X + 1
+
+else :
+    a = line - X + 1
+    b = X
+
+print("%d/%d"%(a,b))
