f73868d0f870050423b673daf8131a46ba355b06
양준하 <100835168+Ori-Gui@users.noreply.github.com>
2023-05-29 14:13:33 +0900

    2023.05.29_2167.py

---
+++ 201902716/2023.05.29_2167.py
@@ -0,0 +1,19 @@
+def main():
+    N, M = map(int, input().split())
+    array = []
+    
+    for _ in range(N):
+        array.append(list(map(int, input().split())))
+        
+    K = int(input())
+    
+    for _ in range(K):
+        i, j, x, y = map(int, input().split())
+        sum = 0
+        for row in range(i-1, x):
+            for col in range(j-1, y):
+                sum += array[row][col]
+        print(sum)
+
+if __name__ == '__main__':
+    main()
