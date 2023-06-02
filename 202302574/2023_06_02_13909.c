a9bee8fa90bab91ddd6c68ebe02987c6a2932c40
011nuyha <comma110@naver.com>
2023-06-02 10:12:14 +0900

    2023_06_02_13909.c

---
+++ 202302574/2023_06_02_13909.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main() {
+	/*1 => 0
+	* 0 => 1
+	* ´ÝÈû = 0, ¿­¸² = 1
+	*
+	int N=15;
+	int arr[100] = {0,};
+	for (int i = 1; i <= N; i++) {
+		for (int j = i; j <= N; j += i) {
+			arr[j] = (arr[j] + 1) % 2;
+		}
+	}
+	for (int i = 1; i <= N; i++) {
+		printf("%d", arr[i]);
+	}
+	*/
+	int N, count = 0;
+	scanf("%d", &N);
+	for (int i = 1; i * i <= N; i++) {
+		count++;
+	}
+	printf("%d", count);
+
+}
