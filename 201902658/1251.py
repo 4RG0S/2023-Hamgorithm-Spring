e98cf164490327906833f77538814ff1984fd3fb
Kimdaemin <56399681+daemin-kim@users.noreply.github.com>
2023-06-18 23:08:30 +0900

    solve 1251

---
+++ 201902658/1251.py
@@ -0,0 +1,18 @@
+word = list(input())
+answer = []
+tmp = []
+
+for i in range(1, len(word) - 1):
+    for j in range(i + 1, len(word) ):
+        a = word[:i]
+        b = word[i:j]
+        c = word[j:]
+        a.reverse()
+        b.reverse()
+        c.reverse()
+        tmp.append(a + b + c)
+
+for a in tmp:
+    answer.append(''.join(a))
+
+print(sorted(answer)[0])
