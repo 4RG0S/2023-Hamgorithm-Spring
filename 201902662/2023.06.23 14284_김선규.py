b322819fd6d915978298946cbc5ca267b6536604
kimseongyu <kskksk0925@naver.com>
2023-06-25 19:41:47 +0900

    Update 2023.06.23 14284_김선규.py

--- 201902662/2023.06.23 14284_김선규.py
+++ 201902662/2023.06.23 14284_김선규.py
@@ -4,13 +4,13 @@ input = sys.stdin.readline
 Inf = float("inf")
 
 def main():
-    n, m = list(map(int, input().split()))
-    edges = {i:{} for i in range(1, n+1)}
+    n, m = map(int, input().split())
+    edges = [[] for _ in range(n+1)]
     for _ in range(m):
-        a, b, c = list(map(int, input().split()))
-        edges[a][b] = c
-        edges[b][a] = c
-    s, t = list(map(int, input().split()))
+        a, b, c = map(int, input().split())
+        edges[a].append((b, c))
+        edges[b].append((a, c))
+    s, t = map(int, input().split())
 
     dist = [Inf for _ in range(n+1)]
     dist[s] = 0
@@ -18,9 +18,10 @@ def main():
     que = [(0, s)]
     while que:
         d, p = heapq.heappop(que)
-        for c in edges[p].keys():
-            if dist[c] > d + edges[p][c]:
-                dist[c] = d + edges[p][c]
+        if dist[p] < d: continue
+        for c, w in edges[p]:
+            if dist[c] > d + w:
+                dist[c] = d + w
                 heapq.heappush(que, (dist[c], c))
     
     print(dist[t])
