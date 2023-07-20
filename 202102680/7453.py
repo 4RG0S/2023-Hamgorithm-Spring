bcc1a5d0073b0b0fea0a0d80687625b61c7daa1f
duswnwn <rita27@naver.com>
2023-07-20 15:37:00 +0900

    23/07/20 7453_합이 0인 네 정수

--- 202102680/7453.py
+++ 202102680/7453.py
@@ -10,16 +10,16 @@ for _ in range(n):
     D.append(d)
 
 # A, B 로 만들 수 있는 합을 모두 구하여 딕셔너리에 몇개 있는 지까지 저장
-left = dict()
+left_sum = dict()
 for a in A:
     for b in B:
-        left[a+b] = left.get(a+b, 0) + 1
+        left_sum[a+b] = left_sum.get(a+b, 0) + 1
 
 count = 0
 
 # C, D로 만들 수 있는 합을 모두 구하여 이에 -1 을 곱한 값이 (더하여 0을 만들 수 있는 값) 딕셔너리에 몇 개 있는 지를 확인하여 count에 더해줌
 for c in C:
     for d in D:
-        count += left.get(-(c+d), 0)
+        count += left_sum.get(-(c+d), 0)
 
 print(count)
