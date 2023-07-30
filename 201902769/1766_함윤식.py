b4f5a002f615c62e2e9f9f5ad2e3d20dafe76b1b
함윤식 <43927226+Ham3798@users.noreply.github.com>
2023-07-30 19:57:36 +0900

    Update 1766_함윤식.py

--- 201902769/1766_함윤식.py
+++ 201902769/1766_함윤식.py
@@ -12,6 +12,7 @@ pq = queue.PriorityQueue()
 for i in range(1, n+1):
     if count[i] == 0:
         pq.put(i)
+# 자신을 가리키는 노드가 없는 경우 우선순위 큐에 넣는다
 
 while not pq.empty():
     cur = pq.get()
@@ -20,3 +21,5 @@ while not pq.empty():
         count[next] -= 1
         if count[next] == 0:
             pq.put(next)
+# 우선순위 큐를 통해 자신을 가리키는 노드가 없는 노드 중 숫자가 작은 경우를 가져옴
+# count = 0인 next 노드를 pq에 넣어준다
