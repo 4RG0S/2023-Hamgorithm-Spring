e2159250b7d8669246fbdceb1daaaed20ce67528
skcpet <130435666+skcpet@users.noreply.github.com>
2023-09-11 12:47:19 +0900

    Create 09.11 -2675.java

---
+++ 202302612/09.11 -2675.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class Main {
+	public static void main(String args[]) {
+		Scanner sc = new Scanner(System.in);
+		
+		int T = sc.nextInt();
+		
+		for(int i =0; i<T; i++) {
+			
+			int R = sc.nextInt();
+			String S = sc.next();
+			
+			
+			for(int j = 0; j<S.length(); j++) {
+				
+				for(int k = 0; k<R; k++) {
+					System.out.print(S.charAt(j));
+				}
+			}
+	
+			System.out.println();
+		}
+	}
+}
